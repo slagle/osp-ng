05707f2d2352a25ece5609a461b5b153c3ecf10d
James Slagle <jslagle@redhat.com>
2023-06-16 07:41:59 -0400

    Update ng-op-scale.sh to account for CSV changes
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-op-scale.sh
+++ scripts/ng-op-scale.sh
@@ -8,6 +8,6 @@ SCALE=${SCALE:-0}
 
 oc project openstack-operators
 
-INDEX=$(oc get csv openstack-operator.v0.0.1 -o json | jq ".spec.install.spec.deployments | map(.name==\"${CONTROLLER_MANAGER}\") | index(true)")
+INDEX=$(oc get csv ${OPERATOR}-operator.v0.0.1 -o json | jq ".spec.install.spec.deployments | map(.name==\"${CONTROLLER_MANAGER}\") | index(true)")
 
-oc patch csv openstack-operator.v0.0.1 --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/'${INDEX}'/spec/replicas", "value":'${SCALE}'}]'
+oc patch csv ${OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/'${INDEX}'/spec/replicas", "value":'${SCALE}'}]'
