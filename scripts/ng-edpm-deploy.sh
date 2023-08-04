c8c4523b8c174687306f409093ac9d0825e76bf3
James Slagle <jslagle@redhat.com>
2023-08-04 12:17:06 -0400

    Fix path var handling and set $DATAPLANE_SINGLE_NODE correctly
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-edpm-deploy.sh
+++ scripts/ng-edpm-deploy.sh
@@ -3,18 +3,18 @@
 set -eux
 
 export NODES=${NODES:-"1"}
-export INSTALL_YAMLS=${INSTALL_YAMLS:-"install_yamls"}
+export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
 export SCRIPTS_DIR=$(dirname $(realpath $0))
 export DATAPLANE_CHRONY_NTP_SERVER=${DATAPLANE_CHRONY_NTP_SERVER:-"clock.redhat.com"}
 
-if [ ! -d $INSTALL_YAMLS ]; then
-    git clone https://github.com/openstack-k8s-operators/install_yamls
+if [ ! -d ${OPENSTACK_K8S_OPERATORS}/install_yamls ]; then
+    git clone https://github.com/openstack-k8s-operators/install_yamls ${OPENSTACK_K8S_OPERATORS}/install_yamls
 fi
 
-pushd $INSTALL_YAMLS
+pushd ${OPENSTACK_K8S_OPERATORS}/install_yamls
 
-if [ ${NODES} = "2" ]; then
-    export DATAPLANE_SINGLE_NODE=false
+if [ ${NODES} = "1" ]; then
+    export DATAPLANE_SINGLE_NODE=true
 fi
 
 make edpm_deploy
