4de5de0b45d2ae4773c091647f947c14a82dc5b7
James Slagle <jslagle@redhat.com>
2023-08-15 09:57:27 -0400

    Hardlink ng-edpm-deploy-prep.sh to ng-edpm-deploy.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-edpm-deploy-prep.sh
+++ scripts/ng-edpm-deploy-prep.sh
@@ -7,6 +7,12 @@ export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
 export SCRIPTS_DIR=$(dirname $(realpath $0))
 export DATAPLANE_CHRONY_NTP_SERVER=${DATAPLANE_CHRONY_NTP_SERVER:-"clock.redhat.com"}
 
+if [ "$0" = "ng-edpm-deploy-prep.sh" ]; then
+    DEPLOY=0
+else
+    DEPLOY=1
+fi
+
 if [ ! -d ${OPENSTACK_K8S_OPERATORS}/install_yamls ]; then
     git clone https://github.com/openstack-k8s-operators/install_yamls ${OPENSTACK_K8S_OPERATORS}/install_yamls
 fi
@@ -15,7 +21,14 @@ pushd ${OPENSTACK_K8S_OPERATORS}/install_yamls
 
 if [ ${NODES} = "1" ]; then
     export DATAPLANE_SINGLE_NODE=true
+else
+    export DATAPLANE_SINGLE_NODE=false
 fi
 
-make edpm_deploy_prep
+if [ "${DEPLOY}" = "0" ]; then
+    make edpm_deploy_prep
+else
+    make edpm_deploy
+fi
+
 popd
