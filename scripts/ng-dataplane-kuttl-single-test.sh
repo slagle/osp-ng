6c37a26d510ad1b7d85b0e40b1d7bd3980b607a3
James Slagle <jslagle@redhat.com>
2023-11-28 12:15:52 -0500

    Add ng-dataplane-kuttl-single-test.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

---
+++ scripts/ng-dataplane-kuttl-single-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+
+TEST=${TEST:-"dataplane-deploy-no-nodes-test"}
+TIMEOUT=${TIMEOUT:-"120"}
+SKIP_DELETE=${SKIP_DELETE:-"--skip-delete"}
+
+oc delete netconfig/netconfig --ignore-not-found
+oc delete dnsmasq/dns --ignore-not-found
+oc delete osdpnodeset --all
+oc delete osdpdeployments --all
+make kuttl-test KUTTL_ARGS="--test ${TEST} --timeout ${TIMEOUT} ${SKIP_DELETE}"
