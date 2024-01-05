59c75b153f67b12bc2a567b905ff663757974ee2
James Slagle <jslagle@redhat.com>
2024-01-05 11:53:10 -0500

    Combine scripts/ng-csv-oo-scale.sh into scripts/ng-csv-scale.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-csv-scale.sh
+++ scripts/ng-csv-scale.sh
@@ -2,12 +2,15 @@
 
 set -eux
 
+CSV_OPERATOR=${CSV_OPERATOR:-"openstack"}
 OPERATOR=${OPERATOR:-"dataplane"}
 CONTROLLER_MANAGER=${CONTROLLER_MANAGER:-"${OPERATOR}-operator-controller-manager"}
 SCALE=${SCALE:-0}
 
 oc project openstack-operators
 
-INDEX=$(oc get csv ${OPERATOR}-operator.v0.0.1 -o json | jq ".spec.install.spec.deployments | map(.name==\"${CONTROLLER_MANAGER}\") | index(true)")
+INDEX=$(oc get csv ${CSV_OPERATOR}-operator.v0.0.1 -o json | jq ".spec.install.spec.deployments | map(.name==\"${CONTROLLER_MANAGER}\") | index(true)")
 
-oc patch csv ${OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/'${INDEX}'/spec/replicas", "value":'${SCALE}'}]'
+oc patch csv ${CSV_OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/'${INDEX}'/spec/replicas", "value":'${SCALE}'}]'
+
+oc project openstack
