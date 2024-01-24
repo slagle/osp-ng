47a41631d1e70ecc7c8a9f9ce4283b4a1d6bf477
James Slagle <jslagle@redhat.com>
2024-01-24 11:37:52 -0500

    Switch to openstack project
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-dataplane-kuttl-single-test.sh
+++ scripts/ng-dataplane-kuttl-single-test.sh
@@ -6,6 +6,7 @@ TEST=${TEST:-"dataplane-deploy-no-nodes-test"}
 TIMEOUT=${TIMEOUT:-"120"}
 SKIP_DELETE=${SKIP_DELETE:-"--skip-delete"}
 
+oc project openstack
 oc delete netconfig/netconfig --ignore-not-found
 oc delete dnsmasq/dns --ignore-not-found
 oc delete osdpnodeset --all
