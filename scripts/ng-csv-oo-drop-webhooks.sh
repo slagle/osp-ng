a74dfb567636689c90854862937e5888e3f29645
James Slagle <jslagle@redhat.com>
2024-02-13 17:54:55 -0500

    Add $OPERATOR support
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-csv-oo-drop-webhooks.sh
+++ scripts/ng-csv-oo-drop-webhooks.sh
@@ -2,11 +2,12 @@
 
 set -eux
 
+OPERATOR=${OPERATOR:-"openstack"}
 WEBHOOK_NAMES=${WEBHOOK_NAMES:-"mopenstackdataplanenodeset.kb.io vopenstackdataplanenodeset.kb.io"}
 
 oc project openstack-operators
 
 for name in ${WEBHOOK_NAMES}; do
-    index=$(oc get csv openstack-operator.v0.0.1 -o json | jq ".spec.webhookdefinitions | map(.generateName==\"${name}\") | index(true)")
-    oc patch csv openstack-operator.v0.0.1 --type='json' -p='[{"op": "remove", "path": "/spec/webhookdefinitions/'${index}'"}]'
+    index=$(oc get csv ${OPERATOR}-operator.v0.0.1 -o json | jq ".spec.webhookdefinitions | map(.generateName==\"${name}\") | index(true)")
+    oc patch csv ${OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "remove", "path": "/spec/webhookdefinitions/'${index}'"}]'
 done
