2dfb82cb636a8c1607e7f80e9f8d7d6b520a61a2
James Slagle <jslagle@redhat.com>
2024-03-28 17:20:21 -0400

    Use $KUTTL_ARGS in scripts/ng-dataplane-kuttl-single-test.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-dataplane-kuttl-single-test.sh
+++ scripts/ng-dataplane-kuttl-single-test.sh
@@ -4,11 +4,15 @@ set -eux
 
 TEST=${TEST:-"dataplane-deploy-no-nodes-test"}
 TIMEOUT=${TIMEOUT:-"120"}
-SKIP_DELETE=${SKIP_DELETE:-"--skip-delete"}
+KUTTL_ARGS="--test ${TEST} --timeout ${TIMEOUT}"
+
+if [ "${SKIP_DELETE}" = "1" ]; then
+    KUTTL_ARGS="${KUTTL_ARGS} --skip-delete"
+fi
 
 oc project openstack
 oc delete netconfig/netconfig --ignore-not-found
 oc delete dnsmasq/dns --ignore-not-found
 oc delete osdpnodeset --all
 oc delete osdpdeployments --all
-make kuttl-test KUTTL_ARGS="--test ${TEST} --timeout ${TIMEOUT} ${SKIP_DELETE}"
+make kuttl-test KUTTL_ARGS="${KUTTL_ARGS}"
