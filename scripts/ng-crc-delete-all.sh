62a0011e86ecf93fd5f4db31d1aa0bb3d77cdf74
James Slagle <jslagle@redhat.com>
2024-03-28 17:19:13 -0400

    Add scripts/ng-crc-delete-all.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

---
+++ scripts/ng-crc-delete-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+
+export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
+
+if [ ! -d ${OPENSTACK_K8S_OPERATORS}/install_yamls ]; then
+    git clone https://github.com/openstack-k8s-operators/install_yamls ${OPENSTACK_K8S_OPERATORS}/install_yamls
+fi
+
+sudo virsh destroy crc
+sudo virsh undefine --remove-all-storage crc
+rm -rf ~/.crc
+rm -f ~/bin/crc
