bb0633d596be04a844e5e12c23cc6c56ddc4541e
James Slagle <jslagle@redhat.com>
2024-04-12 16:10:36 -0400

    Drop new dataplane webhooks, and check if webhook was found
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-csv-drop-webhooks.sh
+++ scripts/ng-csv-drop-webhooks.sh
@@ -3,11 +3,15 @@
 set -eux
 
 OPERATOR=${OPERATOR:-"openstack"}
-WEBHOOK_NAMES=${WEBHOOK_NAMES:-"mopenstackdataplanenodeset.kb.io vopenstackdataplanenodeset.kb.io"}
+WEBHOOK_NAMES=${WEBHOOK_NAMES:-"mopenstackdataplanenodeset.kb.io vopenstackdataplanenodeset.kb.io mopenstackdataplanedeployment.kb.io vopenstackdataplanedeployment.kb.io mopenstackdataplaneservice.kb.io vopenstackdataplaneservice.kb.io"}
 
 oc project openstack-operators
 
 for name in ${WEBHOOK_NAMES}; do
     index=$(oc get csv ${OPERATOR}-operator.v0.0.1 -o json | jq ".spec.webhookdefinitions | map(.generateName==\"${name}\") | index(true)")
-    oc patch csv ${OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "remove", "path": "/spec/webhookdefinitions/'${index}'"}]'
+    if [ ${index} != "null" ]; then
+        oc patch csv ${OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "remove", "path": "/spec/webhookdefinitions/'${index}'"}]'
+    fi
 done
+
+oc project openstack
