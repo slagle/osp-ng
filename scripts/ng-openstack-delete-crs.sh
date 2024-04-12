e0069e378a35855b8da2246297c9d872bdf922ba
James Slagle <jslagle@redhat.com>
2024-04-12 16:11:37 -0400

    scripts/ng-openstack-delete-crs.sh: Also delete secrets
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-openstack-delete-crs.sh
+++ scripts/ng-openstack-delete-crs.sh
@@ -6,16 +6,22 @@ oc project openstack || :
 
 crds=$(oc get --no-headers crd | grep -E 'openstack|rabbitmq' | cut -d' ' -f1)
 
+patch_and_delete () {
+   oc patch ${1} ${2} --type='json' -p='[{"op": "remove", "path": "/metadata/finalizers"}]' || :
+   oc delete --ignore-not-found ${1} ${2}
+}
+
 delete_crs () {
     crs=$(oc get --no-headers ${1} | cut -d' ' -f1)
     for cr in ${crs}; do
-       oc patch ${1} ${cr} --type='json' -p='[{"op": "remove", "path": "/metadata/finalizers"}]'
-       oc delete --ignore-not-found ${1} ${cr}
+		patch_and_delete ${1} ${cr} &
     done
 }
 
 for crd in ${crds}; do
     delete_crs ${crd} &
 done
+
+delete_crs secret
 
 wait
