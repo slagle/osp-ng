9f78dfe2d05f54945e467d52426fbbbaaf9d29c6
James Slagle <jslagle@redhat.com>
2024-04-17 15:48:25 -0400

    Add common.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

---
+++ scripts/common.sh
@@ -0,0 +1,3 @@
+function echored() {
+    echo -e "\033[0;31m$@\033[0m"
+}
--- scripts/ng-csv-scale.sh
+++ scripts/ng-csv-scale.sh
@@ -2,15 +2,22 @@
 
 set -eux
 
+SCRIPT_DIR=$(realpath $(dirname $(basename $0)))
 CSV_OPERATOR=${CSV_OPERATOR:-"openstack"}
 OPERATOR=${OPERATOR:-"dataplane"}
 CONTROLLER_MANAGER=${CONTROLLER_MANAGER:-"${OPERATOR}-operator-controller-manager"}
 SCALE=${SCALE:-0}
 
+source ${SCRIPT_DIR}/common.sh
+
 oc project openstack-operators
 
 INDEX=$(oc get csv ${CSV_OPERATOR}-operator.v0.0.1 -o json | jq ".spec.install.spec.deployments | map(.name==\"${CONTROLLER_MANAGER}\") | index(true)")
 
-oc patch csv ${CSV_OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/'${INDEX}'/spec/replicas", "value":'${SCALE}'}]'
+if [ ${INDEX} != "null" ]; then
+    oc patch csv ${CSV_OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/'${INDEX}'/spec/replicas", "value":'${SCALE}'}]'
+else
+    echored "${CONTROLLER_MANAGER} not found in ${CSV_OPERATOR} csv"
+fi
 
 oc project openstack
