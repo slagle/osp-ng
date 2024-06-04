7c42a9953796fa8655bf589bd036608a372afa87
James Slagle <jslagle@redhat.com>
2024-06-04 17:23:08 -0400

    Use openstack deploy prep
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-openstack-deploy.sh
+++ scripts/ng-openstack-deploy.sh
@@ -5,7 +5,7 @@ set -eux
 export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
 export WAIT_SECONDS=${WAIT_SECONDS:-"60"}
 
-if [ "$(basename $0)" = "ng-openstack-deploy-setup.sh" ]; then
+if [ "$(basename $0)" = "ng-openstack-deploy-prep.sh" ]; then
     DEPLOY=0
 else
     DEPLOY=1
@@ -18,7 +18,7 @@ fi
 pushd ${OPENSTACK_K8S_OPERATORS}/install_yamls
 
 if [ "${DEPLOY}" = "0" ]; then
-    make openstack
+    make openstack_deploy_prep
 else
 
     i=0
