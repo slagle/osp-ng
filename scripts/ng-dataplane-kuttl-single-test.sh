e8297229c99afcceedbd8f309b9e47a153df46c1
James Slagle <jslagle@redhat.com>
2024-06-05 10:41:22 -0400

    Drop dns and netconfig resource delete from kuttl script
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-dataplane-kuttl-single-test.sh
+++ scripts/ng-dataplane-kuttl-single-test.sh
@@ -12,8 +12,6 @@ if [ "${SKIP_DELETE}" = "1" ]; then
 fi
 
 oc project openstack
-oc delete netconfig/netconfig --ignore-not-found
-oc delete dnsmasq/dns --ignore-not-found
 oc delete osdpnodeset --all
 oc delete osdpdeployments --all
 make kuttl-test KUTTL_ARGS="${KUTTL_ARGS}"
