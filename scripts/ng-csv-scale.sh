871e68a244af69f2d959bf67735b0840b99de992
James Slagle <jslagle@redhat.com>
2024-08-05 16:56:01 -0400

    Actually wire up $CSV_VERSION
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-csv-scale.sh
+++ scripts/ng-csv-scale.sh
@@ -13,10 +13,10 @@ source ${SCRIPT_DIR}/common.sh
 
 oc project openstack-operators
 
-INDEX=$(oc get csv ${CSV_OPERATOR}-operator.v0.0.1 -o json | jq ".spec.install.spec.deployments | map(.name==\"${CONTROLLER_MANAGER}\") | index(true)")
+INDEX=$(oc get csv ${CSV_OPERATOR}-operator.${CSV_VERSION} -o json | jq ".spec.install.spec.deployments | map(.name==\"${CONTROLLER_MANAGER}\") | index(true)")
 
 if [ ${INDEX} != "null" ]; then
-    oc patch csv ${CSV_OPERATOR}-operator.v0.0.1 --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/'${INDEX}'/spec/replicas", "value":'${SCALE}'}]'
+    oc patch csv ${CSV_OPERATOR}-operator.${CSV_VERSION} --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/'${INDEX}'/spec/replicas", "value":'${SCALE}'}]'
 else
     echored "${CONTROLLER_MANAGER} not found in ${CSV_OPERATOR} csv"
 fi
