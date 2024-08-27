e7e47427217ad8c0ad95c6b2130dce9c526a1008
James Slagle <jslagle@redhat.com>
2024-08-27 18:09:40 -0400

    Add crc_storage reset to delete-all
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-openstack-delete-all.sh
+++ scripts/ng-openstack-delete-all.sh
@@ -2,7 +2,14 @@
 
 set -eux
 
+export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
 export SCRIPTS_DIR=$(dirname $(realpath $0))
+
+if [ ! -d ${OPENSTACK_K8S_OPERATORS}/install_yamls ]; then
+    git clone https://github.com/openstack-k8s-operators/install_yamls ${OPENSTACK_K8S_OPERATORS}/install_yamls
+fi
+
+pushd ${OPENSTACK_K8S_OPERATORS}/install_yamls
 
 oc project openstack || :
 
@@ -12,3 +19,8 @@ ${SCRIPTS_DIR}/ng-openstack-delete-crds.sh
 ${SCRIPTS_DIR}/ng-openstack-delete-operators.sh
 ${SCRIPTS_DIR}/ng-openstack-delete-namespaces.sh
 ${SCRIPTS_DIR}/ng-pv-reclaim.sh
+
+make crc_storage_cleanup
+make crc_storage
+
+popd
