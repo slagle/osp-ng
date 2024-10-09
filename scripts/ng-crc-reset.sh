00a082d448b2421c96dad6eeee4bfa1595d2889f
James Slagle <jslagle@redhat.com>
2024-10-09 10:39:45 -0400

    Refactor ng-crc-setup.sh, Add ng-crc-reset.sh link
    Signed-off-by: James Slagle <jslagle@redhat.com>

---
+++ scripts/ng-crc-reset.sh
@@ -0,0 +1,1 @@
+ng-crc-setup.sh
--- scripts/ng-crc-setup.sh
+++ scripts/ng-crc-setup.sh
@@ -3,7 +3,9 @@
 set -eux
 
 export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
-export CRC_DELETE=${CRC_DELETE:-"0"}
+export CRC_DELETE=${CRC_DELETE:-"1"}
+export CRC_RESET=${CRC_RESET:-"0"}
+export DOWNLOAD_TOOLS=${DOWNLOAD_TOOLS:-"1"}
 export CPUS=${CPUS:-"16"}
 export MEMORY=${MEMORY:-"32768"}
 export DISK=${DISK:-"80"}
@@ -16,10 +18,17 @@ if [ ! -f "${PULL_SECRET}" ]; then
     exit 1
 fi
 
-if [ "${CRC_DELETE}" = "1" ]; then
-    rm -f ~/bin/crc
-    rm -rf ~/.crc
+if [ "$(basename $0)" = "ng-crc-reset.sh" ]; then
+    CRC_DELETE=0
+    DOWNLOAD_TOOLS=0
+    CRC_RESET=1
 fi
+
+if [ ! -d ${OPENSTACK_K8S_OPERATORS}/install_yamls ]; then
+    git clone https://github.com/openstack-k8s-operators/install_yamls ${OPENSTACK_K8S_OPERATORS}/install_yamls
+fi
+
+pushd ${OPENSTACK_K8S_OPERATORS}/install_yamls
 
 if ! which git; then
     sudo dnf -y install git
@@ -31,18 +40,27 @@ if ! which ansible-playbook; then
     sudo dnf -y install ansible-core
 fi
 
-if [ ! -d ${OPENSTACK_K8S_OPERATORS}/install_yamls ]; then
-    git clone https://github.com/openstack-k8s-operators/install_yamls ${OPENSTACK_K8S_OPERATORS}/install_yamls
+
+pushd devsetup
+
+if [ "${CRC_RESET}" = "1" ]; then
+    make crc_cleanup
 fi
 
-pushd ${OPENSTACK_K8S_OPERATORS}/install_yamls
+if [ "${CRC_DELETE}" = "1" ]; then
+    make crc_cleanup
+    make crc_scrub
+fi
 
-cd devsetup
-make download_tools
+if [ "${DOWNLOAD_TOOLS}" = "1" ]; then
+    make download_tools
+fi
+
 make crc
 eval $(crc oc-env)
 make crc_attach_default_interface
-cd ..
+
+popd
 
 make crc_storage
 
