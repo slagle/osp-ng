8e5e50abbeff612b172248f43712889dc53fb357
James Slagle <jslagle@redhat.com>
2025-01-06 17:01:58 -0500

    Drop shortcut for osaee
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-oo-create-index.sh
+++ scripts/ng-oo-create-index.sh
@@ -4,16 +4,16 @@ set -eux
 
 VERSION=${VERSION:-"18.0.2"}
 PULLSPECS=${PULLSPECS:-"https://download.engineering.redhat.com/rcm-guest/puddles/OpenStack/rhoso-18.0-CI-files/pullspecs-${VERSION}.txt"}
-BUNDLES="$(curl ${PULLSPECS})"
+BUNDLES="$(curl -k ${PULLSPECS} | grep operator-bundle)"
 
 REGISTRY=default-route-openshift-image-registry.apps-crc.testing
 # namespace must match a project name
 REGISTRY_NAMESPACE=openstack-operators
-INDEX_IMAGE=$REGISTRY/$REGISTRY_NAMESPACE/rhoso-operator-index:1.0.0
+INDEX_IMAGE=$REGISTRY/$REGISTRY_NAMESPACE/rhoso-operator-index:${VERSION}
 
 oc registry login --insecure=true
 
-opm index add -u podman --pull-tool podman --tag $INDEX_IMAGE \
+opm index add --build-tool podman --pull-tool podman --tag $INDEX_IMAGE \
     --mode semver \
     --bundles "${BUNDLES}"
 
