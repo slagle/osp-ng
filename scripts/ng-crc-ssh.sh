80c357e1416350a749f9f72f5142b2a7a8185d31
James Slagle <jslagle@redhat.com>
2025-01-16 14:15:13 -0500

    Fix
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-crc-ssh.sh
+++ scripts/ng-crc-ssh.sh
@@ -5,4 +5,4 @@ if [ -z "$ARGS" ]; then
     ARGS="/bin/bash"
 fi
 
-ssh -t -i $HOME/.crc/machines/crc/id_ed25519 core@192.168.130.11 sudo -i /bin/bash
+ssh -t -i $HOME/.crc/machines/crc/id_ed25519 core@192.168.130.11 sudo -i "${ARGS}"
