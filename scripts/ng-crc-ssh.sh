298d019ea36c2e2617487cd647f1ef521d2b8580
James Slagle <jslagle@redhat.com>
2025-01-16 14:13:50 -0500

    Add $ARGS
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-crc-ssh.sh
+++ scripts/ng-crc-ssh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ssh -t -i $HOME/.crc/machines/crc/id_ed25519 core@192.168.130.11 sudo -i
+ARGS="$@"
+if [ -z "$ARGS" ]; then
+    ARGS="/bin/bash"
+fi
+
+ssh -t -i $HOME/.crc/machines/crc/id_ed25519 core@192.168.130.11 sudo -i /bin/bash
