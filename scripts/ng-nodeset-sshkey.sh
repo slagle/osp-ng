771face9f0f7fbe10aba935776c1c5b7f2bef869
James Slagle <jslagle@redhat.com>
2025-01-28 16:55:52 -0500

    Set key permissions
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-nodeset-sshkey.sh
+++ scripts/ng-nodeset-sshkey.sh
@@ -9,6 +9,8 @@ WRITE=${WRITE:-"0"}
 if [ "$WRITE" = "1" ]; then
     oc get secret ${SECRET} -o json | jq -r '.data["ssh-privatekey"]' | base64 -d > ${OPENSTACK_K8S_OPERATORS}/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa
     oc get secret ${SECRET} -o json | jq -r '.data["ssh-publickey"]' | base64 -d > ${OPENSTACK_K8S_OPERATORS}/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa.pub
+    chmod 0600 ${OPENSTACK_K8S_OPERATORS}/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa
+    chmod 0600 ${OPENSTACK_K8S_OPERATORS}/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa.pub
 else
     oc get secret ${SECRET} -o json | jq -r '.data["ssh-privatekey"]' | base64 -d
     oc get secret ${SECRET} -o json | jq -r '.data["ssh-publickey"]' | base64 -d
