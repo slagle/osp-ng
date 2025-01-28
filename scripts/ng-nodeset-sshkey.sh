4b14b7a58ed7af5ca46621789a07f825fac032b8
James Slagle <jslagle@redhat.com>
2025-01-28 16:28:42 -0500

    scripts/ng-nodeset-sshkey.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-nodeset-sshkey.sh
+++ scripts/ng-nodeset-sshkey.sh
@@ -4,5 +4,12 @@ set -eux
 
 NODESET=${NODESET:-"openstack-edpm-ipam"}
 SECRET=${SECRET:-"$(oc get osdpns ${NODESET} -o json | jq -r .spec.nodeTemplate.ansibleSSHPrivateKeySecret)"}
+WRITE=${WRITE:-"0"}
 
-oc get secret ${SECRET} -o json | jq -r '.data["ssh-privatekey"]' | base64 -d
+if [ "$WRITE" = "1" ]; then
+    oc get secret ${SECRET} -o json | jq -r '.data["ssh-privatekey"]' | base64 -d > ${OPENSTACK_K8S_OPERATORS}/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa
+    oc get secret ${SECRET} -o json | jq -r '.data["ssh-publickey"]' | base64 -d > ${OPENSTACK_K8S_OPERATORS}/install_yamls/out/edpm/ansibleee-ssh-key-id_rsa.pub
+else
+    oc get secret ${SECRET} -o json | jq -r '.data["ssh-privatekey"]' | base64 -d
+    oc get secret ${SECRET} -o json | jq -r '.data["ssh-publickey"]' | base64 -d
+fi
