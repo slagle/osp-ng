ef34248e55b30737ab6508fed329b2fac1494203
James Slagle <james.slagle@gmail.com>
2025-04-02 15:45:44 -0400

    scripts/ng-edpm-ansible-build-push.sh
    Signed-off-by: James Slagle <james.slagle@gmail.com>

---
+++ scripts/ng-edpm-ansible-build-push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux
+
+export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
+
+export IMAGE_TAG_BASE=quay.io/jslagle/openstack-ansibleee-runner
+export IMAGE_TAG=bootc
+
+error() {
+    echo "ERROR"
+    popd
+}
+trap error ERR
+
+if ! grep quay.io ${XDG_RUNTIME_DIR}/containers/auth.json; then
+    podman login quay.io
+fi
+
+make openstack_ansibleee_build
+make openstack_ansibleee_push
