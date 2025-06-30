a84e55e6dc4c7965a3bf613f251870630bdc31c1
James Slagle <jslagle@redhat.com>
2025-06-30 14:35:26 -0400

    ng-crc-ssh.sh

--- scripts/ng-crc-ssh.sh
+++ scripts/ng-crc-ssh.sh
@@ -5,4 +5,4 @@ if [ -z "$ARGS" ]; then
     ARGS="/bin/bash"
 fi
 
-ssh -t -i $HOME/.crc/machines/crc/id_ed25519 core@192.168.130.11 sudo -i "${ARGS}"
+ssh -o StrictHostKeyChecking=no -t -i $HOME/.crc/machines/crc/id_ed25519 core@192.168.130.11 sudo -i "${ARGS}"
