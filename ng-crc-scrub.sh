4c732501f991fc4b077851d8c625d406ccbfc6aa
James Slagle <jslagle@redhat.com>
2025-09-04 07:45:56 -0400

    renamed: scripts/ng-crc-delete-all.sh -> ng-crc-scrub.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- ng-crc-scrub.sh
+++ ng-crc-scrub.sh
@@ -8,7 +8,6 @@ if [ ! -d ${OPENSTACK_K8S_OPERATORS}/install_yamls ]; then
     git clone https://github.com/openstack-k8s-operators/install_yamls ${OPENSTACK_K8S_OPERATORS}/install_yamls
 fi
 
-sudo virsh destroy crc || :
-sudo virsh undefine --remove-all-storage crc || :
-rm -rf ~/.crc
-rm -f ~/bin/crc
+pushd ${OPENSTACK_K8S_OPERATORS}/install_yamls/devsetup
+make crc_scrub
+popd
