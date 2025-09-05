823f3b54045c2163e4f33549584789bbf88386d6
James Slagle <jslagle@redhat.com>
2025-09-05 10:19:48 -0400

    cifmw stuff
    Signed-off-by: James Slagle <jslagle@redhat.com>

---
+++ scripts/ng-cifmw-delete.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux
+
+export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
+export SCRIPTS_DIR=$(dirname $(realpath $0))
+export DEEPSCRUB=${DEEPSCRUB:-""}
+
+if [ "${DEEPSCRUB}" = "1" ]; then
+    DEEPSCRUB_ARGS="--tags deepscrub"
+    sudo rm -rf /home/zuul/ci-framework-data
+else
+    DEEPSCRUB_ARGS=""
+fi
+
+ansible-playbook -i custom/inventory.yml \
+    -e cifmw_target_host=hypervisor-1 \
+    reproducer-clean.yml \
+    ${DEEPSCRUB_ARGS}
---
+++ scripts/ng-cifmw-deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eux
+
+export OPENSTACK_K8S_OPERATORS=${OPENSTACK_K8S_OPERATORS:-"$(pwd)"}
+export SCRIPTS_DIR=$(dirname $(realpath $0))
+export FLUSH_CACHE=${FLUSH_CACHE:-"--flush-cache"}
+export RRIP=${RRIP:-""}
+
+if [ -z "${RRIP}" ]; then
+    echo "${RRIP} MUST BE SET TO redhat.registry.io PASSWORD!"
+    exit 1
+fi
+
+pushd ${CIFMW_DIR}
+
+rm -rf ~/ansible.log;
+ansible-playbook \
+  reproducer.yml \
+  -i ${NG_DIR}/ci-framework/custom/inventory.yml \
+  -e @scenarios/reproducers/va-hci.yml \
+  -e @scenarios/reproducers/networking-definition.yml \
+  -i ${NG_DIR}/ci-framework/custom/default-vars.yml \
+  -i ${NG_DIR}/ci-framework/custom/downstream-vars.yml \
+  -e @${NG_DIR}/ci-framework/custom/secrets.yml \
+  -e cifmw_target_host=hypervisor-1 \
+  -e cifmw_deploy_architecture=true \
+  -e cifmw_nolog=false \
+  -e registry_redhat_io_password=${RRIP} \
+  ${FLUSH_CACHE} \
+
+popd
