75a362bbcbab5e0a3e51a112f77f414c994c7022
James Slagle <jslagle@redhat.com>
2025-09-09 07:12:55 -0400

    namespace
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-nodeset-inventory.sh
+++ scripts/ng-nodeset-inventory.sh
@@ -3,5 +3,6 @@
 set -eux
 
 NODESET=${NODESET:-"openstack-edpm-ipam"}
+NAMESPACE=${NAMESPACE:-"openstack"}
 
-oc get secret dataplanenodeset-${NODESET} -o json | jq -r .data.inventory | base64 -d
+oc -n ${NAMESPACE} get secret dataplanenodeset-${NODESET} -o json | jq -r .data.inventory | base64 -d
