87e51b2b526b2435c1765b3c78b56657e934b14b
James Slagle <jslagle@redhat.com>
2025-10-29 14:12:43 -0400

    scripts/ng-host-ssh.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-host-ssh.sh
+++ scripts/ng-host-ssh.sh
@@ -3,10 +3,11 @@
 set -eux
 
 HOST=${1:-"${HOST:-""}"}
+COMMAND=${@:-"sudo -u stack tmux a"}
 
 if [ -z "${HOST}" ]; then
     echo "\$HOST must be set"
     exit 1
 fi
 
-ssh -t root@${HOST} sudo -u stack tmux a
+ssh -t root@${HOST} ${COMMAND}
