555dd652ddcf94693cb9b4b6b8654fdb9f88547f
James Slagle <jslagle@redhat.com>
2025-11-10 12:04:07 -0500

    scripts/ng-cifmw-login.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- scripts/ng-cifmw-login.sh
+++ scripts/ng-cifmw-login.sh
@@ -3,6 +3,6 @@
 set -eux
 
 # PASS=$(ssh -t controller-0 cat .kube/kubeadmin-password || :)
-DEVSCRIPTS_PASS=$(cat /home/${DEVSCRIPTS_USER:-"zuul"}/src/github.com/openshift-metal3/dev-scripts/ocp/ocp/auth/kubeadmin-password)
+DEVSCRIPTS_PASS=$(sudo cat /home/${DEVSCRIPTS_USER:-"zuul"}/src/github.com/openshift-metal3/dev-scripts/ocp/ocp/auth/kubeadmin-password)
 
 oc login -u kubeadmin -p ${DEVSCRIPTS_PASS} https://api.ocp.openstack.lab:6443
